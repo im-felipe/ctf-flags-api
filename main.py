675277a5078c106c2db94ad67f95291a64ffdbb7
Felipe Rodríguez Valdés <181134064+im-felipe@users.noreply.github.com>
2026-09-24 20:48:52 -0500

    Actualizar main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from fastapi import FastAPI
+import sqlite3
 
 app = FastAPI()
 
@@ -15,3 +16,29 @@ class FlagRespuesta(BaseModel):
     flag_hash_o_texto: str
     puntos: int | None
     resuelta: bool
+
+@app.get("/flags", response_model=list[FlagRespuesta])
+def mostrar_flags():
+    conexion = sqlite3.connect("flags.db")
+    conexion.row_factory = sqlite3.Row
+    cursor = conexion.cursor()
+    cursor.execute("""
+        SELECT id, reto, flag_hash_o_texto, puntos, resuelta FROM flags
+    """)
+    filas = cursor.fetchall()
+    cursor.close()
+    conexion.close()
+    return [dict(fila) for fila in filas]
+
+@app.post("/flags", response_model=FlagRespuesta)
+def insertar_flag(flag: Flag):
+    conexion = sqlite3.connect("flags.db")
+    conexion.row_factory = sqlite3.Row
+    cursor = conexion.cursor()
+    cursor.execute("INSERT INTO flags (reto, flag_hash_o_texto, puntos, resuelta) VALUES (?, ?, ?, ?)", (flag.reto, flag.flag_hash_o_texto, flag.puntos, int(flag.resuelta)))
+    conexion.commit()
+    nuevo_id = cursor.lastrowid
+    cursor.close()
+    conexion.close()
+    return {"id": nuevo_id, "reto": flag.reto, "flag_hash_o_texto": flag.flag_hash_o_texto, "puntos": flag.puntos, "resuelta": flag.resuelta}
+    
